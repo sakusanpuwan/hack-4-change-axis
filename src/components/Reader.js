e3cfbca622327f6e6d4ecfc990ab1ca610556d9d
Migle <ambrazeviciutemigle@gmail.com>
2023-10-12 08:45:13 +0100

    adding static placeholder text to format

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -1,6 +1,14 @@
 const Reader = () => {
     return (
-        <h1>Reader</h1>
+        <div>
+            <h1>Reader</h1>
+            <div>
+                <h2>Input text</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum lorem eget libero scelerisque mattis. Aenean sagittis rutrum nulla. Phasellus finibus, ipsum at condimentum porttitor, justo justo tempus leo, non porta dolor tortor nec orci. Vestibulum blandit neque sit amet aliquam ultrices. Nam tincidunt, nunc id ornare pretium, eros orci dapibus lorem, vel feugiat felis eros ut enim. Ut enim tellus, vulputate nec enim in, gravida dictum est. Pellentesque venenatis tortor quis hendrerit interdum. Pellentesque sagittis velit at quam viverra, ac vehicula mauris dictum. Sed mi enim, molestie quis velit non, accumsan interdum ante. Donec ultrices mi eget nibh aliquet.</p> 
+                <h2>Output text</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum lorem eget libero scelerisque mattis. Aenean sagittis rutrum nulla. Phasellus finibus, ipsum at condimentum porttitor, justo justo tempus leo, non porta dolor tortor nec orci. Vestibulum blandit neque sit amet aliquam ultrices. Nam tincidunt, nunc id ornare pretium, eros orci dapibus lorem, vel feugiat felis eros ut enim. Ut enim tellus, vulputate nec enim in, gravida dictum est. Pellentesque venenatis tortor quis hendrerit interdum. Pellentesque sagittis velit at quam viverra, ac vehicula mauris dictum. Sed mi enim, molestie quis velit non, accumsan interdum ante. Donec ultrices mi eget nibh aliquet.</p> 
+            </div>
+        </div>
     )
 }
 
