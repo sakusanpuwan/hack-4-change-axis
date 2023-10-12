3325147e43922970c18cb1dd606ad5e88c5e8a0d
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-10-12 16:55:21 +0100

    implemented gpt request + input form

--- src/components/ReaderText.js
+++ src/components/ReaderText.js
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import fetchGPTResponse from "../services/FetchGPTResponse";
+import { PropagateLoader } from "react-spinners";
+
 const ReaderText = () => {
+
+    const [prompt,setPrompt] = useState("");
+    const [response,setResponse] = useState();
+    const [isLoading,setIsLoading] = useState()
+
+    const handleClick = async () => {
+        setIsLoading(true)
+        const response = await fetchGPTResponse(prompt);
+        setResponse(response);
+        setIsLoading(false)
+    } 
+
+
     return (
-        <h1>ReaderText</h1>
+        <div className="reader-text-container">
+            <h1>ReaderText</h1>
+            <form>
+                <textarea rows={6} cols={50} onChange={(event) => setPrompt(event.target.value)} placeholder="Enter text here..."></textarea>
+                <br></br>
+                <button onClick={() => {handleClick()}} value="submit">Submit</button>    
+            </form>
+            <br></br>
+            {isLoading === true ? <PropagateLoader color="white"/> :<p>{response}</p> }
+
+        </div>
+        
     )
 }
 
