8f3de9044ccb72b9d6a03e7c7883dc1570c6b220
Migle <ambrazeviciutemigle@gmail.com>
2023-10-13 09:02:51 +0100

    adding bionic reader text formatting functionality

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -1,15 +1,54 @@
+import { useState } from "react";
+import {textVide} from "text-vide";
+import ReactHtmlParser from 'react-html-parser';
+import './Reader.css';
+
 const Reader = () => {
+
+    const [inputText, setInputText] = useState([]);
+    const [outputText, setOutputText] = useState([]);
+    const [fixationPoint, setFixationPoint] = useState([]);
+
+
+    const handleChange = (event) => {
+        setInputText(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const bionic = textVide(inputText);
+        setOutputText(bionic);
+    }
+
+    const handleFixation= (event) => {
+        setFixationPoint(event.target.value)
+        const bionic = textVide(inputText, {fixationPoint: fixationPoint});
+        setOutputText(bionic);
+    }
+    
+    console.log(textVide(inputText))
+
     return (
         <div>
             <h1>Reader</h1>
-            <div>
-                <h2>Input text</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum lorem eget libero scelerisque mattis. Aenean sagittis rutrum nulla. Phasellus finibus, ipsum at condimentum porttitor, justo justo tempus leo, non porta dolor tortor nec orci. Vestibulum blandit neque sit amet aliquam ultrices. Nam tincidunt, nunc id ornare pretium, eros orci dapibus lorem, vel feugiat felis eros ut enim. Ut enim tellus, vulputate nec enim in, gravida dictum est. Pellentesque venenatis tortor quis hendrerit interdum. Pellentesque sagittis velit at quam viverra, ac vehicula mauris dictum. Sed mi enim, molestie quis velit non, accumsan interdum ante. Donec ultrices mi eget nibh aliquet.</p> 
-                <h2>Output text</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin fermentum lorem eget libero scelerisque mattis. Aenean sagittis rutrum nulla. Phasellus finibus, ipsum at condimentum porttitor, justo justo tempus leo, non porta dolor tortor nec orci. Vestibulum blandit neque sit amet aliquam ultrices. Nam tincidunt, nunc id ornare pretium, eros orci dapibus lorem, vel feugiat felis eros ut enim. Ut enim tellus, vulputate nec enim in, gravida dictum est. Pellentesque venenatis tortor quis hendrerit interdum. Pellentesque sagittis velit at quam viverra, ac vehicula mauris dictum. Sed mi enim, molestie quis velit non, accumsan interdum ante. Donec ultrices mi eget nibh aliquet.</p> 
+            <div className="input-text">
+                <form onSubmit={handleSubmit}>
+                    <textarea type="text" id="inputTextBox" value={inputText} onChange={handleChange}/>
+                    <button id="textFormatButton">Generate!</button>
+                </form>
             </div>
+            <div className="bionic-section">
+                <h2>BIONIC Output</h2>
+                <p>Change fixation point:</p>
+                <input type="range" id="fixationPoint" min="1" max="5" value={fixationPoint} onChange={handleFixation}/>
+            </div>        
+
+            <p id="outputText">{ReactHtmlParser(outputText)}</p>
         </div>
     )
 }
 
 export default Reader;
+
+// TEMPLATE TEXT
+// It is imperative to recognise the significance of acknowledging neurodiversity in the workplace. This entails acknowledging and accommodating individuals with neurological differences, such as autism, ADHD, and dyslexia, among others. By embracing neurodiversity, organisations can foster a more inclusive and diverse work environment, which can lead to increased productivity, creativity, and innovation.
