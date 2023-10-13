128bf5c3bf39fc348a0c36eb0918de418a52e5f8
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-10-13 20:12:48 +0100

    added format checkpoints to prompt:btn error?

--- src/components/ReaderText.js
+++ src/components/ReaderText.js
@@ -1,29 +1,42 @@
 import { useState } from "react";
 import fetchGPTResponse from "../services/FetchGPTResponse";
 import { PropagateLoader } from "react-spinners";
+import ReadFormatOptions from "./ReadFormatOptions";
 
 const ReaderText = () => {
 
     const [prompt,setPrompt] = useState("");
+    const [input,setInput] = useState("");
     const [response,setResponse] = useState();
     const [isLoading,setIsLoading] = useState()
 
     const handleClick = async () => {
         setIsLoading(true)
+        setPrompt((prevPrompt) => `${prevPrompt} .The text is:${input}`)
         const response = await fetchGPTResponse(prompt);
         setResponse(response);
-        setIsLoading(false)
+        setTimeout(async () => {
+            const response = await fetchGPTResponse(prompt);
+            setResponse(response);
+          }, 1000);
+        setIsLoading(false);
+        // setPrompt("");
     } 
+
+    const updatePrompt = (formatter) => {
+        setPrompt((prevPrompt) => prevPrompt + ","+ formatter)
+    }
 
 
     return (
         <div className="reader-text-container">
             <h1>ReaderText</h1>
             <form>
-                <textarea rows={6} cols={50} onChange={(event) => setPrompt(event.target.value)} placeholder="Enter text here..."></textarea>
+                <textarea rows={6} cols={50} onChange={(event) => setInput(event.target.value)} placeholder="Enter text here..."></textarea>
                 <br></br>
                 <button onClick={() => {handleClick()}} value="submit">Submit</button>    
             </form>
+            <ReadFormatOptions updatePrompt = {updatePrompt}/>
             <br></br>
             {isLoading === true ? <PropagateLoader color="white"/> :<p>{response}</p> }
 
@@ -33,3 +46,4 @@ const ReaderText = () => {
 }
 
 export default ReaderText;
+
