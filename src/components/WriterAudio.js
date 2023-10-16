169cf0b8298a90711d267310c89b55d796c1278c
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-10-16 19:21:47 +0100

    added speech to text function

--- src/components/WriterAudio.js
+++ src/components/WriterAudio.js
@@ -1,7 +1,29 @@
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
 const WriterAudio = () => {
+
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+    
+    if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+    }
+    
     return (
+    <div>
         <h1>WriterAudio</h1>
-    )
+        <p>Microphone: {listening ? 'on' : 'off'}</p>
+        <button onClick={() => {SpeechRecognition.startListening({continuous:true})}}>Start</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button onClick={resetTranscript}>Reset</button>
+        <p>{transcript}</p>
+    </div>
+    );
 }
 
 export default WriterAudio;
