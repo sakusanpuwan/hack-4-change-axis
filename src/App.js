9e1260dee334bed313b39408f9ddd08580ea101a
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-10-17 16:57:31 +0100

    restructured app to return only navbar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
-import ReaderText from './components/ReaderText';
-import ReaderAudio from './components/ReaderAudio';
-import WriterText from './components/WriterText';
-import WriterAudio from './components/WriterAudio';
+import NavBar from './components/NavBar';
+
+
 
 
 function App() {
   return (
     <div className="App">
-      <nav className='sidebar'>
-      <h2>AXIS</h2>
-        <div className='profile'> 
-            <img src='https://m.media-amazon.com/images/I/41jLBhDISxL._AC_UF1000,1000_QL80_.jpg' width={'40px'} style= {{borderRadius: "50%"}}></img>
-            <h3>Name</h3>
-        </div>
-        <ul className='navbar'>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/reader-text">Reader Text</Link></li>
-          <li><Link to="/reader-audio">Reader Audio</Link></li>
-          <li><Link to="/writer-text">Writer Text</Link></li>
-          <li><Link to="/writer-audio">Writer Audio</Link></li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route path='/reader-text' element={<ReaderText/>}/>
-        <Route path='/reader-audio' element={<ReaderAudio/>}/>
-        <Route exact path='/writer-text' element={<WriterText/>}/>
-        <Route exact path='/writer-audio' element={<WriterAudio/>}/>
-      </Routes>
-
+        <NavBar/>
     </div>
   );
 }
