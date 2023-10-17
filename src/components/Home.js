7ed59740fd162c4c2d5934054b2bf6976e2661aa
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-10-17 16:56:55 +0100

    added login + register comp to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import '../styling/Home.css'
+import Login from './Login';
+import Register from './Register';
 
-const Home = () => {
+const Home = ({handleLoginStatus}) => {
     return (
         <div className='home-container'>
             <h1>Home</h1>
             <p>Explain what this app does. Features/Aim/Credits</p>
+            <Login handleLoginStatus = {handleLoginStatus}/>
+            <Register handleLoginStatus = {handleLoginStatus}/>
         </div>
         
     )
