6add1b23e397ba3f4a2776543e4e9953f5712e9f
Migle <ambrazeviciutemigle@gmail.com>
2023-10-23 23:33:25 +0100

    adding export to pdf functionality for audio transcription

--- src/components/WriterAudio.js
+++ src/components/WriterAudio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
+import { saveAs } from 'file-saver';
 
 const WriterAudio = () => {
 
@@ -13,6 +15,28 @@ const WriterAudio = () => {
     if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
     }
+
+    // Export as PDF
+        // consider adding - font, font size, colour params as chosen by the user
+    const exportPDF = async () => {
+        const pdfDoc = await PDFDocument.create();
+        const page = pdfDoc.addPage(); 
+            // consider adding multiple pages dynamically as text size increases
+        const {width, height} = page.getSize();
+        const timesNewRoman = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+        page.drawText(transcript, {
+            x: 50,
+            y: height - 5 * 10, // page height - 5 * font size
+            size: 10,
+            color: rgb(0, 0, 0),
+            maxWidth: width - 100, // to allow text to wrap
+            lineHeight: 20,
+            font: timesNewRoman
+        });
+        const pdfBytes = await pdfDoc.save(); // into an array of bytes making up a PDF file
+        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+        saveAs(blob, 'transcribedText.pdf');
+    };
     
     return (
     <div>
@@ -22,6 +46,9 @@ const WriterAudio = () => {
         <button onClick={SpeechRecognition.stopListening}>Stop</button>
         <button onClick={resetTranscript}>Reset</button>
         <p>{transcript}</p>
+        <div className="export-section">
+            <button id="pdfButton" onClick={exportPDF}>Export PDF</button>
+        </div>
     </div>
     );
 }
